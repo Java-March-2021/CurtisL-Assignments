84d2f11af2e8c7e7cc3ecd6482916a796fde0334
curtistkd123 <curtislewis1989@yahoo.com>
2021-03-15 21:37:45 -0400

    first commit

---
+++ 02-java-oop/05-object-master-pt1/src/objectMaster/HumanTest.java
@@ -0,0 +1,14 @@
+package objectMaster;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human h1 = new Human();
+		Human h2 = new Human();
+		
+		h1.attack(h2);
+		System.out.println(h2.getHealth());
+		
+		System.out.println(h1.toString());
+	}
+}
